e8e58a870049f34eb4116a3bf5f92a9f49a29d56
Santiago Macias <smaciasr@unal.edu.co>
2024-03-07 14:39:10 -0500

    ejercicio incompleto

---
+++ modulo 2/Recorrido arcoíris.py
@@ -0,0 +1,12 @@
+N = int(input())
+palabras = list(map(str,input().split(", ")))
+palabra_nueva=[]
+
+for i in range(int(N//2)):
+    palabra_nueva.append(palabras[i])
+    palabra_nueva.append(palabras[N-i-1])
+
+    
+for _ in palabra_nueva:
+    cadena= "".join(_)
+    print(cadena, end="")
