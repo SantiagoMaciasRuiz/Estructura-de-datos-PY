71a4ea02238feca0f1aa63242e37c49923928ae3
Santiago Macias <smaciasr@unal.edu.co>
2024-03-13 15:16:05 -0500

    termine el punto

--- modulo 3/PrimiConjunto.py
+++ modulo 3/PrimiConjunto.py
@@ -1,12 +1,33 @@
+def calcular_divisores(numero):
+    divisores = []
+    for i in range(1, numero + 1):
+        if numero % i == 0:
+            divisores.append(i)
+    return divisores
+    
+
 C = int(input())
 for _ in range(C):
     valores = list(map(int,input().split()))
     lista = list(map(int,input().split()))
-    primos = []
-    for i in range (valores[1]):
-        if valores[1] % lista[i] == 0:
-            num=lista[i]
-            primos.append(num)
+    divisores = calcular_divisores(valores[1])
+    intervalo = len(divisores)
+    for i in divisores:
+        if i in lista:
+            intervalo = intervalo
+        else:
+            intervalo -= 1
+    if intervalo == len(divisores):
+        print("Es PrimiConjunto")
+        
+    else:
+        print("No es PrimiConjunto")
+ 
+        
+        
+        
+        
+ 
         
            
 
